4fe15af5721a31ba99d058e6f1b8cd83d0f4e522
Russell <russell.hoy@gmail.com>
2016-10-16 15:21:24 -0400

    interim commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Project from './Project.js';
+
+import wantToVideo from './videos/ifyouwantto.mp4';
+import duncanPalmerVideo from './videos/drawing-duncan.mp4';
+import takeTheStairsVideo from './videos/take-the-elevator.mp4';
+
 import duncanPalmer from './images/duncan_palmer.png';
 import takeStairs from './images/take-the-stairs.png';
 import wantToHaveTo from './images/want-to-have-to.png';
-// import Grade from 'grade-js';
 
 class App extends Component {
   render() {
@@ -19,9 +23,9 @@ class App extends Component {
           </header>
         </div>
         <div className="slope-container">
-          <Project theme="#005962" previewImage={duncanPalmer} title="Drawing Duncan Palmer" category="Short Film" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
-          <Project theme="#482935" previewImage={takeStairs} title="Take the Elevator" category="Experimental" movieSrc="https://player.vimeo.com/video/148242729"></Project>
-          <Project theme="#fad49e" previewImage={wantToHaveTo} title="If you want to, you have to" category="Orchestra" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
+          <Project theme="#0e2300" previewImage={duncanPalmer} video={duncanPalmerVideo} title="Drawing Duncan Palmer" category="Short Film" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
+          <Project theme="#482935" previewImage={takeStairs} video={takeTheStairsVideo} title="Take the Elevator" category="Experimental" movieSrc="https://player.vimeo.com/video/148242729"></Project>
+          <Project theme="#fad49e" previewImage={wantToHaveTo} video={wantToVideo} title="If you want to, you have to" category="Orchestra" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
         </div>
           <p className="contact-me">
             Lets collaborate
