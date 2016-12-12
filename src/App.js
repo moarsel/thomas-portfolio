406809e278b5292447204301522c741e19939a55
Russell <russell.hoy@gmail.com>
2016-12-11 20:32:26 -0500

    Manage state in parent and youtube vids

--- src/App.js
+++ src/App.js
@@ -2,23 +2,74 @@ import smoothScroll from 'smoothscroll-polyfill';
 smoothScroll.polyfill();
 
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
-// import Project from './Project.js';
+
 import Feature from './Feature.js';
 
-// import wantToVideo from './videos/ifyouwantto.mp4';
-// import duncanPalmerVideo from './videos/drawing-duncan.mp4';
-// import takeTheStairsVideo from './videos/take-the-elevator.mp4';
-
-// import duncanPalmer from './images/duncan_palmer.png';
-// import wantToHaveTo from './images/want-to-have-to.png';
-// import takeStairs from './images/take-the-stairs.png';
-
-// <Project theme="#0e2300" previewImage={duncanPalmer} video={duncanPalmerVideo} title="Drawing Duncan Palmer" category="Short Film" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
-// <Project theme="#482935" previewImage={takeStairs} video={takeTheStairsVideo} title="Take the Elevator" category="Experimental" movieSrc="https://player.vimeo.com/video/148242729"></Project>
-// <Project theme="#fad49e" previewImage={wantToHaveTo} video={wantToVideo} title="If you want to, you have to" category="Orchestra" description="yep yep yep"  movieSrc="https://player.vimeo.com/video/148242729"></Project>
-
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {activeFeature: -1};
+    
+    this.featuredContent = [
+      {
+        theme:"#0e2300",
+        video:"https://www.youtube.com/watch?v=JfmyXwpFLbc",
+        title:"Drawing Duncan Palmer",
+        category:"Short Film",
+        description: `Putent nostrud ut quo. Ignota pertinacia ex usu, ad elit interpretaris 
+        vim. Ei ius tation labore, vis ea senserit reprimique appellantur. Cum te iuvaret 
+        corrumpit maiestatis, ad vis erat epicurei oportere. Cu vel officiis consequuntur, no
+        simul nominavi prodesset vis, id his nibh aeterno. Eum ad pertinax constituam, sed ubique 
+        scripta signiferumque ei, adhuc tation facilisis an nec.` 
+      },
+      {
+        theme:"#0e2300",
+        video:"https://www.youtube.com/watch?v=wVjGmzqW4Lg",
+        title:"If you want to, you have to",
+        category:"Orchestra",
+        description: `Putent nostrud ut quo. Munere abhorreant usu te, ex per delenit delectus
+        salutatus, summo simul sea an. Ignota pertinacia ex usu, ad elit interpretaris vim. 
+        Ei ius tation labore, vis ea senserit reprimique appellantur. Cum te iuvaret corrumpit 
+        maiestatis, ad vis erat epicurei oportere. Cu vel officiis consequuntur, no simul 
+        nominavi prodesset vis, id his nibh aeterno. Eum ad pertinax constituam, sed ubique 
+        scripta signiferumque ei, adhuc tation facilisis an nec.` 
+      }
+    ]
+  }
+
+  toggleActive(index){
+    this.setState({activeFeature: index});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    let videoContainer = ReactDOM.findDOMNode(this.refs.activeFeature);
+    if(videoContainer && videoContainer.offsetTop && prevState !== this.state) {
+      window.scroll({
+        top: videoContainer.offsetTop - 25,
+        behavior: 'smooth'
+      });
+    }    
+  }
+
+  renderFeature(feature, index){
+    const attrs = {
+      key: index,
+      theme: feature.theme,
+      video: feature.video,
+      title: feature.title,
+      description: feature.description
+    }
+
+    let active = this.state.activeFeature === index;
+
+    if(active) {
+      attrs.ref = "activeFeature";
+    }
+    return (<Feature {...attrs} activeFeature={active} toggleActive={this.toggleActive.bind(this, index)}/>)
+  }
+
   render() {
     return (
       <div className="App">
@@ -32,8 +83,13 @@ class App extends Component {
         <p className="card-byline">
           Lorem ipsum dolor sit amet, an vidit omittam vulputate ius. Velit denique accumsan ei qui, utinam admodum omnesque ne eum, an mel aperiam labores mandamus. Purto quidam cu sit, ex eam quando primis interesset.
         </p>
-        <Feature theme="#0e2300" video={"JfmyXwpFLbc"} title="Drawing Duncan Palmer" category="Short Film" description="Putent nostrud ut quo. Ignota pertinacia ex usu, ad elit interpretaris vim. Ei ius tation labore, vis ea senserit reprimique appellantur. Cum te iuvaret corrumpit maiestatis, ad vis erat epicurei oportere. Cu vel officiis consequuntur, no simul nominavi prodesset vis, id his nibh aeterno. Eum ad pertinax constituam, sed ubique scripta signiferumque ei, adhuc tation facilisis an nec."  movieSrc="https://player.vimeo.com/video/148242729"></Feature>
-        <Feature theme="#0e2300" video={"wVjGmzqW4Lg"} title="If you want to, you have to" category="Orchestra" description="Ex velit augue voluptua quo, vel modus fuisset ea. Eirmod eripuit molestiae ea duo, duo praesent salutandi scriptorem cu. Lorem omittam repudiandae mea ut, ea sale reque eos. Munere abhorreant usu te, ex per delenit delectus salutatus, summo simul sea an. Fabulas atomorum scriptorem sea ei, stet quando aperiam nam no, congue iriure cum ad."  movieSrc="https://player.vimeo.com/video/148242729"></Feature>
+        {
+          this.featuredContent.map(
+            (feature, index) => {
+              return this.renderFeature(feature, index);
+            }
+          )
+        }
         <p className="contact-me">
           
         </p>
--- src/Feature.js
+++ src/Feature.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player'
 import './Feature.css';
 
 class Feature extends Component {
 
   constructor() {
     super();
-    this.state = {isOpen: false};
     this.handleScroll = this.handleScroll.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-  };
+  }
 
   componentDidMount(){
     window.addEventListener('scroll', this.handleScroll);
@@ -17,76 +15,37 @@ class Feature extends Component {
     // let projectVideo = this.refs.projectVideo;
     // projectVideo.volume = 0;
     // projectVideo.controls = false;
-  };
+  }
 
   handleScroll(e) {
-    let projectVideo = this.refs.projectVideo.internalPlayer;
+    // let projectVideo = this.refs.projectVideo.internalPlayer;
     let featureBounds = this.refs.featureBlurb.getBoundingClientRect();
     let isVisible = (featureBounds.top + 200 < window.innerHeight) && (featureBounds.bottom - 540 > 0);
     let featureCenter = featureBounds.top - featureBounds.top / 2;
     featureCenter *= Math.sign(featureCenter);
     featureCenter = 1 - featureCenter / 150;
     // projectVideo.style.transform = "translateY: #{22+ featureCenter}px";
-    // console.log(featureCenter, `HSLA(20,20,#{featureCenter}, 1)`);
-    // console.log(`HSLA(20,20,${featureCenter * 100}, 1)`);
 
     if (featureCenter > 0) {
       // console.log(`RGBA(22,33, ${featureCenter * 100}, 1)`);
       // document.body.style.backgroundColor = `HSLA(222,${featureCenter * 10}, 100, 1)`;
     }
-    
 
-
-    // Math.floor(Math.abs(x));
-
-    // cool effect
-    // projectVideo.currentTime = featureCenter * 10;
-
-
-    
-
-
-    if (isVisible && !this.state.isOpen) {
-      projectVideo.playVideo();
-    } else if (!isVisible && !this.state.isOpen){
+    if (isVisible && !this.props.activeFeature) {
+      // projectVideo.playVideo();
+    } else if (!isVisible && !this.props.activeFeature){
       this.closeFeature();
     }
-  };
-
-  handleClick(e){
-    this.openFeature();
-  }
-
-  openFeature(){
-    let projectVideo = this.refs.projectVideo.internalPlayer;
-    let videoContainer = this.refs.videoContainer;
-    
-    this.setState({isOpen: true});
-    projectVideo.seekTo(0);
-    projectVideo.unMute();
-    projectVideo.setOption("controls", 1);
-
-    projectVideo.playVideo();
-    console.log(videoContainer, videoContainer.offsetTop)
-
-    window.scroll({
-      top: videoContainer.offsetTop - 25,
-      behavior: 'smooth'
-    });
-
-    setTimeout(function(){
-      // projectVideo.controls = true;
-    }, 1200);
   }
 
   closeFeature(){
-      let projectVideo = this.refs.projectVideo.internalPlayer;
+      // let projectVideo = this.refs.projectVideo.internalPlayer;
 
 
       // this.setState({isOpen: false})
       // projectVideo.pause();
-      projectVideo.volume = 0;
-      projectVideo.controls = false;
+      // projectVideo.mute();
+      // projectVideo.setOption("controls", 0);
 
       // maintain scroll position on close...
       // var tempScrollTop = window.scrollY;
@@ -97,57 +56,49 @@ class Feature extends Component {
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
-  };
-
-  onReady(event) {
-    let video = event.target;
-    video.playVideo();
-    video.mute();
-    video.controls = false;
   }
 
   render() {
-
  
-    let activeClass = this.state.isOpen ? 'projectOpen' : 'projectClosed';
+    let activeClass = this.props.activeFeature ? 'projectOpen' : 'projectClosed';
     const videoOpts = {
-      width: '100%',
       playerVars: { // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
-        controls: this.state.isOpen ? 1 : 0,
+        controls: this.props.activeFeature ? 1 : 0,
         showinfo: 0,
         rel:0,
-        modestbranding: 1
+        modestbranding: 1,
+        videoId: this.props.video
       }
     }
 
 
     return (
-      <div className={activeClass}>
+      <section className={activeClass}>
         <div ref="featureBlurb" className="featureContainer">
-          
-          
+          <div>{this.props.activeFeature}</div>
           <div className="videoElement" ref="videoContainer">
-            <YouTube
-            ref="projectVideo"
-            videoId={this.props.video}
-            opts={videoOpts}
-            onReady={this.onReady}
-            />
+            <ReactPlayer
+              url={this.props.video}
+              ref="projectVideo"
+              playing={this.props.activeFeature}
+              youtubeConfig={videoOpts}
+              width="100%"
+              height="60vh"/>
           </div>
 
 
-          <div className="feature" onClick={this.handleClick}>
+          <div className="feature" onClick={this.props.toggleActive}>
             <div className="feature-text">
               <div className="feature-title">{this.props.title}</div>
               <div className="feature-category">{this.props.category}</div>
             </div>
             <div className="feature-description">
-            {this.props.description}
+              {this.props.description}
             </div>
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
